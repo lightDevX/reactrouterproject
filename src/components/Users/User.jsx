0d87456addf63f5e6a6100554f97e2f72899e472
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-03-23 05:44:28 +0600

    Navigate route used

--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -1,19 +1,32 @@
-import { Link } from "react-router";
+import { useNavigate } from "react-router";
 
 const User = ({ user }) => {
   const { name, phone, email, id } = user;
-  console.log(user);
+
+  const navigate = useNavigate();
+
+  const handleShowDetails = () => {
+    navigate(`/user/${id}`);
+  };
+
   return (
     <div className="space-y-3.5 rounded-2xl border border-amber-200 p-4">
       <h1 className="text-xl font-semibold text-red-400">Name: {name}</h1>
       <p>Phone: {phone}</p>
       <p>Email: {email}</p>
-      <Link
+      {/* <Link
         to={`/user/${id}`}
         className="rounded-2xl bg-neutral-800/30 px-6 py-2.5"
       >
         Show more details
-      </Link>
+      </Link> */}
+
+      <button
+        onClick={handleShowDetails}
+        className="rounded-2xl bg-neutral-800/30 px-6 py-2.5"
+      >
+        Show more details
+      </button>
     </div>
   );
 };
--- src/components/Users/UserDetail.jsx
+++ src/components/Users/UserDetail.jsx
@@ -1,13 +1,25 @@
-import { useLoaderData } from "react-router";
+import { useLoaderData, useNavigate } from "react-router";
 
 const UserDetail = () => {
   const userDetail = useLoaderData();
 
   const { name } = userDetail;
 
+  const navigate = useNavigate();
+
+  const handleBackBtn = () => {
+    navigate(-1);
+  };
+
   return (
     <div>
       <h2>User Name: {name}</h2>
+      <button
+        onClick={handleBackBtn}
+        className="rounded-2xl bg-neutral-800/30 px-6 py-2"
+      >
+        Back
+      </button>
     </div>
   );
 };
